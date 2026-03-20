f4444c7ff9ea0fc1a9481364d09fa8f26aadffe4
Demon1cAng3l <wolfy240607@gmail.com>
2026-03-20 10:59:32 +0100

    Added 5 files and finished assignment3.php

--- student_assignments/assignment4.php
+++ student_assignments/assignment4.php
@@ -4,3 +4,10 @@
 
     // Opdracht 4: Toon alle personages die een man zijn, kaal zijn en een bril hebben.
     // Tip: Combineer meerdere voorwaarden in je if-statement.
+
+foreach ($characterDataset as $name => $data) {
+    $f = $data['features'];
+    if ($f['man'] === 1 && $f['glasses'] === 1 && $f['bald'] === 1) {
+        echo $name . ' is een kale man met een bril.<br>';
+    }
+}
