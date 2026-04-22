1eda02cb6a48a386dd9f644092479e007f1f411c
Demon1cAng3l <wolfy240607@gmail.com>
2026-04-22 15:19:26 +0200

    Changed 8.css a bit

--- student_assignments/assignment5.php
+++ student_assignments/assignment5.php
@@ -5,3 +5,10 @@
     // Opdracht 5: Toon alle personages met hun afbeelding.
     // Tip: De images staan in de map '../images/' en hebben de naam van het personage + '.png'.
     // Gebruik HTML om de lijst en images weer te geven.
+foreach ($characterDataset as $key => $value) {
+    if ($key === "_feature_order") {
+        continue;
+    }
+    $img = "../images/{$key}.png";
+    echo $key . "<br>" . "<img alt='s' src=$img ><br>";
+}
